2eef5e1b4cc3b1d0e563684aa6395452a528cc9f
fahadnizz <fahadnizz1@gmail.com>
2023-11-07 12:04:39 +0530

    responsive work is remaining

---
+++ js/first.js
@@ -0,0 +1,78 @@
+// console.log('fahad');
+// document.write('fahad');
+// var a = 5;
+
+// let age = 14;
+// let HasVoterCard = 'yes';
+// if (age<=14 && HasVoterCard == 'yes'){
+//     alert("You Can Vote!")
+// } else {
+//     alert("You Can't Vote!")
+// }
+
+// let food = {
+//     name:"pizza",
+//     price:"300rs",
+//     size:"6inch"
+// }
+
+// const {name,price,size} = food
+// var {name: dishname,price:dishprice,size:dishsize} = food;
+// console.log(dishname)
+
+//destructure
+
+// const food = [
+//     {
+//       Name:"Apple",
+//       Type:"fruit",
+//       Price:100
+//     },
+//     {
+//       Name:"pizza",
+//       Type:"junk food",
+//       Price:370
+//     },
+//     {
+//       Name:"Burger",
+//       Type:"junk food",
+//       Price:310
+//     },
+//     {
+//       Name:"grapes",
+//       Type:"fruit",
+//       Price:50
+//     },
+//     {
+//      Name:"mango",
+//      Type:"fruit",
+//      Price:50
+//    }
+//   ]
+
+//   const foodnames = food.map((FOOD)=> FOOD.Name);    // map
+//   console.log(foodnames);
+
+//   const foodnames1 = food.filter((nameFood) => nameFood.Type === 'junk food' );    //filter
+//   console.log(foodnames1);
+
+// const foodrange = food.filter((rangeFood) => rangeFood.Price>=300);         // filter price range
+// console.log(foodrange)
+
+
+// const foodname = food.filter((fahad)=> fahad.Type==='fruit');         // filter type
+// console.log(foodname);
+
+// const foodname = food.sort((a,b) =>a.Price - b.Price);
+// console.log(foodname);                                                //sort for ascending and descending 
+
+
+// let fahad = "Engineer";
+// let nizami = "Teacher";
+// console.log(fahad);
+// [fahad, nizami] = [nizami, fahad]
+// console.log(fahad)
+
+
+
+
